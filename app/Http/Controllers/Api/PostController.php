c34ac5927498d5012359b32908041b6f65b3734a
Sara-Barbi <barbisara02@gmail.com>
2022-03-22 16:45:54 +0100

    modifica fileweb api & PostController api funge

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,25 +1,27 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Post;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-   //-----------post list
-
    public function index(){
+
+   //  COME RICHIAMO TUTTI I POST
+   
+   $posts= Post::all();
+   return response()->json($posts);
+
+}
+   //------------ESEMPIO BASE
    //     return response()->json([
    //        'name'=> 'Simone',
    //        'surname'=>'Antonelli'
    //    ]);
    
-   
-   //----------COME RICHIAMO TUTTI I POST-------------
-   
-   $post= Post::all();
-   return response()->json($posts);
 
    //----------RECUPERO SECONDO ID DELLA CATEGORIA SU TUTTI I RECORD
    
@@ -27,7 +29,6 @@ class PostController extends Controller
    //$posts_filtered= Post::where("category_id",2)-get();
    //return response()->json($posts_filtered);
 
-   }
 
    //-----------RICHIESTA DINAMICA CATEGORIA
    
